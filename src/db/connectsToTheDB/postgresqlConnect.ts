2c9833736c2eaaf926db91fc6d1c56049ad4291f
Korchemnyi Nikita <worflersst@proton.me>
2025-03-25 19:09:21 +0200

    add: insertAllData function and tableDB enum

--- src/db/connectsToTheDB/postgresqlConnect.ts
+++ src/db/connectsToTheDB/postgresqlConnect.ts
@@ -1,15 +1,29 @@
 import pg from 'pg'
 const {Client} = pg
 
+export enum postgresTableName {
+     Regions = 'Regions',
+     Territories = 'Territories',
+     Employees = 'Employees',
+     Shippers = 'Shippers' ,
+     Supplies = 'Supplies' ,
+     Category = 'Category' ,
+     Products = 'Products' ,
+     Customers = 'Customers' ,
+     Orders = 'Orders' ,
+     OrderDetails = 'OrderDetails' ,
+     EmployeeTerritories = 'EmployeeTerritories'
+}
+
+export const client = new Client({
+    user: 'postgres',
+    password: '34385734',
+    host: 'localhost',
+    port: 3000,
+    database: 'northwind_traders',
+})
 
 export const runDB = async () => {
-    const client = new Client({
-        user: 'postgres',
-        password: '34385734',
-        host: 'localhost',
-        port: 3000,
-        database: 'northwind_traders',
-    })
     try {
         await client.connect()
         console.log("Connected to the database");
